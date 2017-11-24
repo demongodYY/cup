972525e95fcddf97f656c447404a73c0c9dab233
qq yy <qq yy>
2017-11-25 02:23:26 +0800

    fix code

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -7,6 +7,7 @@
     </textarea>
     <div id="myCanvas">
     </div>
+    <input type="file" id="fileup"/>
     <el-button plain>上一步</el-button>
     <el-button plain>下一步</el-button>
     <el-button plain>清空重做</el-button>
@@ -21,7 +22,8 @@ export default {
   data () {
     return {
       textarea: '',
-      textShow: false
+      textShow: false,
+      layer: ''
     }
   },
   methods: {
@@ -33,9 +35,55 @@ export default {
       })
       console.log(dataUrl)
     },
-    addShapes (layer) {
+    inputText (text) {
+      const textarea = this.$refs.textarea
+      const textPosition = text.position()
+      textarea.value = text.text()
+      textarea.style.position = 'absolute'
+      textarea.style.fontSize = `${text.fontSize()}px`
+      textarea.style.fontFamily = text.fontFamily()
+      textarea.style.width = `${text.width()}px`
+      textarea.style.height = `${text.height()}px`
+      textarea.style.top = textPosition.y + 'px'
+      textarea.style.left = textPosition.x + 'px'
+      textarea.style['z-index'] = 1
+      this.textShow = true
+      textarea.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+          text.text(textarea.value)
+          this.layer.draw()
+          this.textShow = false
+        }
+      })
+    },
+    drawImage (dataUrl) {
+      const imgObj = new Image()
+      imgObj.src = dataUrl
+      imgObj.onload = () => {
+        const img = new Konva.Image({
+          name: 'img',
+          image: imgObj,
+          width: 100,
+          height: 100
+        })
+        this.layer.add(img)
+        this.layer.draw()
+      }
+    },
+    uploadImage () {
+      const fileUp = document.getElementById('fileup')
+      fileUp.onchange = () => {
+        const fr = new FileReader()
+        fr.onload = (e) => {
+          this.drawImage(e.target.result)
+          // console.log(e.target.result)
+        }
+        fr.readAsDataURL(fileUp.files[0])
+      }
+    },
+    addShapes () {
       const circle = new Konva.Circle({
-        x: layer.getWidth() / 2,
+        x: this.layer.getWidth() / 2,
         y: 200,
         scaleX: 2,
         scaleY: 2,
@@ -44,7 +92,7 @@ export default {
         strokeWidth: 3
       })
       const text = new Konva.Text({
-        x: (layer.getWidth() / 2) - 100,
+        x: (this.layer.getWidth() / 2) - 100,
         y: 400,
         width: 200,
         height: 100,
@@ -55,29 +103,11 @@ export default {
         fontFamily: 'Calibri'
       })
       text.on('click', () => {
-        const textarea = this.$refs.textarea
-        const textPosition = text.position()
-        textarea.value = text.text()
-        textarea.style.position = 'absolute'
-        textarea.style.fontSize = `${text.fontSize()}px`
-        textarea.style.fontFamily = text.fontFamily()
-        textarea.style.width = `${text.width()}px`
-        textarea.style.height = `${text.height()}px`
-        textarea.style.top = textPosition.y + 'px'
-        textarea.style.left = textPosition.x + 'px'
-        textarea.style['z-index'] = 1
-        this.textShow = true
-        textarea.addEventListener('keydown', (e) => {
-          if (e.keyCode === 13) {
-            text.text(textarea.value)
-            layer.draw()
-            this.textShow = false
-          }
-        })
+        this.inputText(text)
       })
-      layer.add(circle)
-      layer.add(text)
-      layer.draw()
+      this.layer.add(circle)
+      this.layer.add(text)
+      this.layer.draw()
     }
   },
   mounted () {
@@ -87,6 +117,7 @@ export default {
       height: 600
     })
     const bgLayer = new Konva.Layer()
+    this.layer = bgLayer
     stage.add(bgLayer)
     this.$root.bus.$on('bgChange', (text) => {
       bgLayer.find('.background').destroy()
@@ -103,7 +134,8 @@ export default {
         this.saveImage(stage)
       }
     })
-    this.addShapes(bgLayer)
+    this.addShapes()
+    this.uploadImage()
   }
 }
 </script>
